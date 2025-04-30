b643c06f1dabdc51b1b04727c5917b248f73cd58
akmal <m_akmal_hisyam@soc.uum.edu.my>
2025-04-30 09:23:13 +0800

    change apk name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:edumi_app/main.dart';
+import 'package:edumi_mobile/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
